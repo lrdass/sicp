64f633571578f568d1b8178350c1ba41f0a3ccca
lrdass <66750963+lrdass@users.noreply.github.com>
2021-05-14 12:29:11 -0300

    Add files via upload

---
+++ exercise_2_35.rkt
@@ -0,0 +1,21 @@
+#lang sicp
+
+(define (fringe L)
+  (cond  ((null? L )nil)
+       ((not (pair? (car L))) L)
+      (else (append (fringe (car L)) (fringe (cdr L))))))
+;; fringe become a map?
+
+(define (acc op initial items)
+  (if (null? items)
+      initial
+      (op (car items)
+          (acc op initial (cdr items)))))
+
+
+(define (count-leaves t)
+  (acc (lambda (x y) (+ 1 y))
+       0
+       (fringe t)))
+
+(count-leaves (list (list (list 1 2) (list 3 4)) (list 5 6)))
