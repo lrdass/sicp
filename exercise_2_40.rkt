fa7cc3d2c4f6a239aa34e9fe96bfd2f86a98ae92
lrdass <66750963+lrdass@users.noreply.github.com>
2021-05-20 14:31:04 -0300

    Add files via upload

---
+++ exercise_2_40.rkt
@@ -0,0 +1,36 @@
+#lang sicp
+(define (enumerate low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate (+ 1 low) high))))
+
+(define (acc op init seq)
+  (if (null? seq)
+      init
+      (op (car seq)
+          (acc op init (cdr seq)))))
+
+(define (flatmap proc seq)
+  (acc append nil (map proc seq)))
+
+(define (filter pred seq)
+  (cond ((null? seq) nil)
+        ((pred (car seq)) (cons (car seq) (filter pred (cdr seq))))
+        (else (filter pred (cdr seq)))))
+
+
+(define (pair-permutation p) (list (car (cdr p)) (car p)))
+
+(define (pairs-are-equal? p1 p2)
+  (and (= (car p1) ( car (cdr p2)))
+       (= (car (cdr p1)) (car p2))))
+
+(pairs-are-equal? (list 1 2) (list 2 1))
+(filter (lambda(pair) (not (pairs-are-equal? pair (pair-permutation pair))))
+        (list (list 1 2) (list 3 4) (list 2 1)))
+
+(filter (lambda (pair)(not (pairs-are-equal? pair (pair-permutation pair))))
+        (flatmap (lambda (i)
+                   (map (lambda(j) (list i j))
+                        (enumerate 1 19)))
+                 (enumerate 2 20)))
